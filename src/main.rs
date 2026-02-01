a6d6acbd892da667ca3fffe02dfd13f4659b84b6
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-02-01 00:12:19 +0000

    advance most functionality

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,31 @@
 //!
 //! Main file of the crate.
 
+use crate::utils::{
+  create_backup, create_file, get_daily_filename, get_data_path, get_editor, remove_file, run_editor
+};
+
 mod utils;
 
 fn main() -> anyhow::Result<()> {
-  let data_path = utils::get_data_path()?;
-  utils::create_data_dir(&data_path)?;
+  let mut main_path = get_data_path()?;
+  utils::create_data_dir(&main_path)?;
+  main_path.push(get_daily_filename());
+
+  let backup_path = main_path.with_extension("backup");
+  main_path.add_extension("txt");
+
+  let editor = get_editor()?;
+
+  // Creates a backup. If the editor fails, remove the changed and uses the backup.
+  if main_path.is_file() {
+    create_backup(&main_path, &backup_path)?;
+    run_editor(&editor, &main_path)?;
+    remove_file(&backup_path)?;
+  } else {
+    create_file(&main_path)?;
+    run_editor(&editor, &main_path)?;
+  }
+
   Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -7,13 +7,15 @@
 
 use std::env;
 use std::fs;
+use std::fs::File;
 use std::path::{Path, PathBuf};
+use std::process::Command;
 
 use anyhow::Context;
+use time::OffsetDateTime;
 
 const DEFAULT_XDG_DATA_PATH: &str = ".local/share";
-
-pub const PROGRAM_DIR_DATA_NAME: &str = "diary-cli";
+const PROGRAM_DIR_DATA_NAME: &str = "diary-cli";
 
 /// Returns the path where the program data will be stored.
 pub fn get_data_path() -> anyhow::Result<PathBuf> {
@@ -22,7 +24,6 @@ pub fn get_data_path() -> anyhow::Result<PathBuf> {
     // if XDG_DATA_HOME is set, use that path.
     result = PathBuf::from(x);
     eprintln!("- Data path found: {}", result.to_str().unwrap());
-
   } else {
     // If not, fallback to default '$HOME/.local/share'
     let home_raw_path = env::var("HOME").context("Failed to retrieve $HOME value")?;
@@ -45,4 +46,57 @@ pub fn create_data_dir(path: &Path) -> anyhow::Result<()> {
     eprintln!("- The directory was sucesfully created");
   }
   Ok(())
+}
+
+/// Returns the daily date filename, with extension '.txt'.
+pub fn get_daily_filename() -> String {
+  let date = OffsetDateTime::now_utc().date().to_string();
+  eprintln!("- Retrieved daily date filename: {}", date);
+  date
+}
+
+/// Creates a backup file of the daily file.
+pub fn create_backup(original: &Path, backup: &Path) -> anyhow::Result<()> {
+  debug_assert!(original.is_file() && !backup.is_file());
+  File::create(backup).context("Failed to create the backup file")?;
+  fs::copy(original, backup).context("Failed to copy the original to the backup")?;
+  eprintln!("- Created backup file in: {}", backup.to_str().unwrap());
+  Ok(())
+}
+
+/// Creates a file in path.
+pub fn create_file(path: &Path) -> anyhow::Result<()> {
+  debug_assert!(!path.is_file());
+  File::create(path).context("Failed to create the daily file")?;
+  eprintln!("- Created file in: {}", path.to_str().unwrap());
+  Ok(())
+}
+
+/// Return the value of $EDITOR.
+pub fn get_editor() -> anyhow::Result<String> {
+  let value = env::var("EDITOR").context("Failed to retrieve env var $EDITOR")?;
+  eprintln!("- Retrieved $EDITOR value: {}", value);
+  Ok(value)
+}
+
+/// Run the editor child process.
+pub fn run_editor(editor: &str, path: &Path) -> anyhow::Result<()> {
+  let mut command = Command::new(editor);
+  command.arg(path);
+  eprintln!("- Command to run: {:?}", command);
+  let mut child_handle = command.spawn().context("Failed to spawn editor child process")?;
+  child_handle.wait().context("Failed to wait the child")?;
+  eprintln!("- Editor finished correctly");
+  Ok(())
+}
+
+pub fn remove_file(path: &Path) -> anyhow::Result<()> {
+  if !path.is_file() {
+    eprintln!("- File {} doesn´t exit, skipping delete", path.to_str().unwrap());
+    Ok(())
+  } else {
+    remove_file(path)?;
+    eprintln!("- File {} correctly removed", path.to_str().unwrap());
+    Ok(())
+  }
 }
