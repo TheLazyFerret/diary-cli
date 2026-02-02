410baaa803e857f151d6f1a50ee65703100a37a3
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2026-02-02 15:31:41 +0000

    add list and show in args

--- src/args.rs
+++ src/args.rs
@@ -11,21 +11,44 @@ use clap::Parser;
 
 /// Static global variable for enabling debug output.
 pub static DEBUG: Mutex<bool> = Mutex::new(false);
+pub static LIST: Mutex<bool> = Mutex::new(false);
+pub static SHOW: Mutex<u32> = Mutex::new(!0);
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
   /// Show verbose output.
-  #[arg(short, long, action = clap::ArgAction::Count)]
-  debug: u8,
+  #[arg(short, long)]
+  debug: bool,
+  
+  /// Show a list with all the entries.
+  #[arg(short, long, conflicts_with = "show")]
+  list: bool,
+  
+  /// Open an specific day.
+  #[arg(short, long, conflicts_with = "list")]
+  show: Option<u32>
+  
 }
 
 /// Parse and set the program arguments configurations.
 pub fn set_arguments() {
   let arguments = Args::parse();
   // Verbose
-  if arguments.debug > 0 {
-    let mut locker = DEBUG.lock().expect("Error locking the Mutex");
+  if arguments.debug == true {
+    let mut locker = DEBUG.lock().expect("Error locking the mutex");
     *locker = true;
+  }
+
+  // List
+  if arguments.list == true {
+    let mut locker = LIST.lock().expect("Error locking the mutex");
+    *locker = true;
+  }
+  
+  // Show
+  if let Some(x) = arguments.show {
+    let mut locker = SHOW.lock().expect("Error locking the mutex");
+    *locker = x;
   }
 }
